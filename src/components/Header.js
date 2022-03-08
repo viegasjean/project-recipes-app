a8114393a0411adc65c28d84e4dd9879dbf0999b
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-03-07 21:40:47 -0300

    <feat>(Header) Show input field on click in search button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileImg from '../images/profileIcon.svg';
 import searchImg from '../images/searchIcon.svg';
+import recipesContext from '../context/recipesContext';
 
 export default function Header({ name, isSearched }) {
+  const { isOpenedSearch: { isOpened },
+    openOrCloseSearchInput } = useContext(recipesContext);
   return (
     <section>
       <Link to="/profile">
@@ -12,7 +15,16 @@ export default function Header({ name, isSearched }) {
       </Link>
       <h2 data-testid="page-title">{ name }</h2>
       {isSearched
-        ? <img data-testid="search-top-btn" alt="search icon" src={ searchImg } /> : null}
+        ? (
+          <button type="button" onClick={ openOrCloseSearchInput }>
+            <img
+              data-testid="search-top-btn"
+              alt="search icon"
+              src={ searchImg }
+            />
+          </button>)
+        : null}
+      {isOpened ? <input data-testid="search-input" type="text" /> : null}
     </section>
   );
 }
--- src/context/recipesProvider.js
+++ src/context/recipesProvider.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import recipesContext from './recipesContext';
 
 function RecipesProvider({ children }) {
+  const [isOpenedSearch, setOpened] = useState({
+    isOpened: false,
+  });
+  const openOrCloseSearchInput = () => {
+    setOpened((prevState) => ({
+      isOpened: !prevState.isOpened,
+    }));
+  };
   return (
-    <recipesContext.Provider value={ 0 }>
+    <recipesContext.Provider value={ { isOpenedSearch, openOrCloseSearchInput } }>
       {children}
     </recipesContext.Provider>
   );
